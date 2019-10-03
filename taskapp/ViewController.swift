b13f76584801230000b29c02e0dfcc12e8d1ac57
mikako.kinugawa <kinugawa0807@yahoo.co.jp>
2019-10-03 09:17:03 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by mikako kinugawa on 2019/10/03.
+//  Copyright © 2019 mikako.kinugawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
