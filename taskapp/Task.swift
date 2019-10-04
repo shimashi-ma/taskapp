338cc7cd8e8c2c8d95310c102a7aa00b6422186d
mikako.kinugawa <kinugawa0807@yahoo.co.jp>
2019-10-04 20:54:25 +0900

    実装完了。

---
+++ taskapp/Task.swift
@@ -0,0 +1,31 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by mikako kinugawa on 2019/10/03.
+//  Copyright © 2019 mikako.kinugawa. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
